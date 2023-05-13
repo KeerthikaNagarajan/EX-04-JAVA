8b56ccc80dbb1fd37b193f8f58a299f05775c437
Keerthika Nagarajan <93427089+KeerthikaNagarajan@users.noreply.github.com>
2023-05-13 09:12:44 +0530

    Add files via upload

---
+++ Ex4.java
@@ -0,0 +1,14 @@
+// Exp-4
+// Java program to print the even numbers between 1-20
+public class Ex4
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=0;i<=20;i++)
+        {
+            System.out.print(+i+"\n");
+            i+= 1;
+        }
+    }
+}
